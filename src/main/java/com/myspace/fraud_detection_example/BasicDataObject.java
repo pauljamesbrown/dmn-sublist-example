25b901c8e6f8f82f28fe4c17033ee941d1b063cc
rhpamAdmin <>
2021-07-22 10:26:59 +0100

    {/src/main/java/com/myspace/fraud_detection_example/BasicDataObject.java}

--- src/main/java/com/myspace/fraud_detection_example/BasicDataObject.java
+++ src/main/java/com/myspace/fraud_detection_example/BasicDataObject.java
@@ -18,6 +18,8 @@ public class BasicDataObject implements java.io.Serializable {
 
 	private java.time.LocalDateTime fieldfour;
 
+	private java.lang.Integer amount;
+
 	public BasicDataObject() {
 	}
 
@@ -61,14 +63,24 @@ public class BasicDataObject implements java.io.Serializable {
 		this.fieldfour = fieldfour;
 	}
 
+	public java.lang.Integer getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(java.lang.Integer amount) {
+		this.amount = amount;
+	}
+
 	public BasicDataObject(java.lang.String transactionId,
 			java.lang.String fieldtwo, java.lang.String dateField,
-			java.util.Date fieldthree, java.time.LocalDateTime fieldfour) {
+			java.util.Date fieldthree, java.time.LocalDateTime fieldfour,
+			java.lang.Integer amount) {
 		this.transactionId = transactionId;
 		this.fieldtwo = fieldtwo;
 		this.dateField = dateField;
 		this.fieldthree = fieldthree;
 		this.fieldfour = fieldfour;
+		this.amount = amount;
 	}
 
 }
